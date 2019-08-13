76e205f6565bb0e3eed7fdc93e93a48db240fb1f
nabildroid <pni20156789@gmail.com>
2019-08-13 23:18:56 +0100

    flatContent for make the return of parseContent() flat string

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php 
-include "config.php";
-
-
-$user=new Profile;
-$user->login("c_user=100009747405464; xs=20:lGaNLcxxejf-1g:2:1557504986");
-
-
-
-
-// $user->wall->all();
-$post=new Post(338556417070774,$user);
-$post->fetch_info();
-
-var_dump($post->comments(0)[2]->reply("this comment has been published through sublime text editor how and why ,who knows"));
-
-
-
-
-
-
-
-function info($txt){
-	echo "<h3>".$txt."</h3>";
-}
-
-
-?>
+include __DIR__."/config.php";
+ ?>
