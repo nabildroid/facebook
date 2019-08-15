321c47cbe02470a66529412af0aa340c39c93624
nabildroid <pni20156789@gmail.com>
2019-08-15 10:36:26 +0100

    get the id of mine account through the cookie it self

--- oop/account.php
+++ oop/account.php
@@ -14,6 +14,9 @@ class Account extends system{
 	}
 	public function login($cookie){
 		$this->cookie=$cookie;
+		preg_match_all("/c_user=.(\d).*?(?=;)/",$cookie,$id);
+		if(isset($id[0][0]))$id=substr($id[0][0],7);
+		$this->profile->info["id"]=$id;
 	}
 	
 	protected function parseMenu($html){
