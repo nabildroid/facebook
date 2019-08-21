9e1cc274da06059ccaaddcd8156c6961fb2d79d6
nabildroid <pni20156789@gmail.com>
2019-08-21 14:23:29 +0100

    update messages class and add multi section of messages

--- oop/messages.php
+++ oop/messages.php
@@ -6,26 +6,42 @@ class Messages extends common{
 		$this->parent=$parent;
 		parent::__construct();
 	}
-	private function fetch_info(){
-		$this->http("/messages");
-	}
-	public function all(){
-		$this->fetch_info();
+	public function parseMessages(){
 		$msgs=filter($this->dom("<table"),function($table){
 			return strpos($table,"/messages/read/")!==false;
 		})[0];
 		$msgs=array_map(function($m){
 			$link=dom($m,"<a",1)[0];
-			$friend_name=$link[0];
 			$id=$link[1]["href"];//contain friend id and 
+			preg_match_all("/[\d]+/",urldecode($id),$id);
+			if($id[0][0]==$this->root->profile->info["id"])
+				$id=$id[0][1];
+			else $id=$id[0][0];
+
 			$snippet=dom($m,"<span")[0];
 			return new Message([
-				"id"=>$id,
-				"friend"=>$friend_name,
+				"friend"=>intval($id),
 			],$this);
 		},$msgs);
 		return $msgs;
 	}
+	public function recent(){
+		$this->http("/messages");
+		return $this->parseMessages();
+	}
+	public function request(){
+		$this->http("/messages/?folder=pending");
+		return $this->parseMessages();
+	}
+	public function unread(){
+		$this->http("/messages/?folder=unread");
+		return $this->parseMessages();
+	}
+	public function other(){
+		$this->http("/messages/?folder=other");
+		return $this->parseMessages();
+	}
+
 								
 
 }
