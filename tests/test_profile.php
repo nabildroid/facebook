ae2c52de5ac98c400e9b0b219776b59e7224d6fe
nabildroid <pni20156789@gmail.com>
2019-08-29 11:32:46 +0100

    update test_profile.php

--- tests/test_profile.php
+++ tests/test_profile.php
@@ -44,31 +44,46 @@ $mine=$user->profile;
 	// $pendingRequests[0]->rejectUserRequest();
 
 # user profile
+$user=new Profile($user,100001226455561);
 
-$user=new Profile($user,"algorithm19");
-
-var_dump("ID: ".$user->getId());
-var_dump("bio: ".$user->getBio());
-if($user->getPicture("profile"))
-var_dump("picture profile: ".$user->getPicture("profile")->getId());
-if($user->getPicture("cover"))
-var_dump("picture cover: ".$user->getPicture("cover")->getId());
+// var_dump("ID: ".$user->getId());
+// var_dump("bio: ".$user->getBio());
+// if($user->getPicture("profile"))
+// var_dump("picture profile: ".$user->getPicture("profile")->getId());
+// if($user->getPicture("cover"))
+// var_dump("picture cover: ".$user->getPicture("cover")->getId());
 ##posts
-var_dump("--------------POSTS");
-for ($i=0; $i <3 ; $i++) { 
-	var_dump("---page:".$i);
-	$posts=$user->posts($i);
-	foreach ($posts as $post) {
-		var_dump($post->getId());
+// var_dump("--------------POSTS");
+// for ($i=0; $i <3 ; $i++) { 
+// 	var_dump("---page:".$i);
+// 	$posts=$user->posts($i);
+// 	foreach ($posts as $post) {
+// 		var_dump($post->getId());
+// 	}
+// }
+
+##one post comments
+$post=$user->posts(0)[1];
+var_dump($post->getId());
+for ($i=0; $i <2 ; $i++) { 
+	$cmts=$post->comments($i);
+	var_dump("#############PAGE: ".$i);
+	foreach ($cmts as $cmt) {
+		var_dump("------------");
+		var_dump(flatContent($cmt->getContent()));
+		var_dump("------------");
 	}
 }
 
+
+
+
 ##friends
-var_dump("--------------friends");
-$friends=$user->friends();
-foreach ($friends as $friend) {
-	var_dump("friend id: ".$friend->getId());
-}
+// var_dump("--------------friends");
+// $friends=$user->friends();
+// foreach ($friends as $friend) {
+// 	var_dump("friend id: ".$friend->getId());
+// }
 ##actions
 
 // $user->sendFriendRequest();
