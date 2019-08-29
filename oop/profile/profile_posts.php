54b6bac1bef32d920f38e2e28622ad508b7e399c
nabildroid <pni20156789@gmail.com>
2019-08-29 15:48:03 +0100

    update posts in profile

--- oop/profile/profile_posts.php
+++ oop/profile/profile_posts.php
@@ -2,35 +2,31 @@
 trait profile_posts{
 	public function posts($page=0){
 		$this->fetch();
+
+		//prepare the url
+		if(!$this->childs["items"])
+			$next=$this->id;
+		else $next=$this->childs["next_page"];
+
+		for ($i=count($this->childs["items"]);$i <=$page; $i++) {
+			if(!$next)break;
+			$this->http($next."?v=timeline");
+			$content=$this->splitPosts();
+			$tempPosts=[];
+			foreach ($content["posts"] as $html){
+				$post=new Post($this);
+				$post->fixHttpResponse($html,null);
+				$tempPosts[]=$post;
+			}
+
+			$this->childs["items"]=array_merge($this->childs["items"],[$tempPosts]);
+			$this->childs["next_page"]=$content["next_page"];
+			$next=$this->childs["next_page"];
+		}
+		
 		if(isset($this->childs["items"][$page]))
 			return $this->childs["items"][$page];
-		else{
-
-			if(!$this->childs["items"])
-				$next=$this->id;
-			else $next=$this->childs["next_page"];
-
-			//prepare the url
-
-			for ($i=count($this->childs["items"]);$i <=$page; $i++) {
-				if(!$next)break;
-				$this->http($next."?v=timeline");
-				$content=$this->splitPosts();
-				$tempPosts=[];
-				foreach ($content["posts"] as $html){
-					$post=new Post($this);
-					$post->fixHttpResponse($html,null);
-					$tempPosts[]=$post;
-				}
-
-				$this->childs["items"]=array_merge($this->childs["items"],[$tempPosts]);
-				$this->childs["next_page"]=$content["next_page"];
-				$next=$this->childs["next_page"];
-			}
-			if(isset($this->childs["items"][$page]))
-				return $this->childs["items"][$page];
-			else return [];
-		}
+		else return [];
 	}
 
 	private function splitPosts(){
