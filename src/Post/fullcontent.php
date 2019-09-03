7b67611d187f25b666bb63fbe602b0c4b49a6c2f
nabildroid <pni20156789@gmail.com>
2019-09-03 14:22:40 +0100

    improve Post fullcontent

--- src/Post/fullcontent.php
+++ src/Post/fullcontent.php
@@ -1,16 +1,13 @@
 <?php 
 namespace Facebook\Post;
 use Facebook\Utils\Util;
-use Facebook\Utils\Content;
 trait fullcontent{
 	//get Full content
 	public function fullContent(){
 		$this->fetch();
 		if($this->content){
-			$content=$this->content;
-			//note: use findDom to check if "more" exist and where
-			$flat=Content::flat($content);
-			if(Util::instr($flat,"...More")){
+			//note: bad criteria
+			if(Util::findInTree($this->content,["content"=>"More"])){
 				$this->fetch(1);
 			} 
 			return $this->content;
