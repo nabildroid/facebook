86e077e462509c001873206d7dafe0f52bc5ada3
nabil <pni20156789@gmail.com>
2019-09-09 10:41:49 +0000

    execute wall test

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php 
+<?php
 namespace Facebook;
 require __DIR__ . '/vendor/autoload.php';
+
+$user=new Account;
+$user->login("datr=AZFIXQTAaRVWnVEXWdFAmepU; sb=AZFIXQzp0jJDVApP-vpXwKow; m_pixel_ratio=1; locale=en_US; x-referer=eyJyIjoiL2dyb3Vwcy80OTI3MDMyOTc1MjU0MDYiLCJoIjoiL2dyb3Vwcy80OTI3MDMyOTc1MjU0MDYiLCJzIjoibSJ9; wd=1351x672; c_user=100011210498274; xs=39%3App8x3AWfzHJfeA%3A2%3A1568024818%3A629%3A615; fr=0SWZUCvrUfbSKcUnY.AWX_z5P0C80BDkkzK1h6J_e0QM0.BdSI35.hv.AAA.0.0.Bddijx.AWX8HMB3");
+
+
+$test="wall";
+
+include "tests/test".$test.".php";
+
 ?>
