05c5803f068e18a537bc9642e17308fbc580a1d4
nabil <pni20156789@gmail.com>
2019-09-13 12:07:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
 namespace Facebook;
 require __DIR__ . '/vendor/autoload.php';
-
-$user=new Account;
-$user->login("datr=AZFIXQTAaRVWnVEXWdFAmepU; sb=AZFIXQzp0jJDVApP-vpXwKow; m_pixel_ratio=1; locale=en_US; x-referer=eyJyIjoiL2dyb3Vwcy80OTI3MDMyOTc1MjU0MDYiLCJoIjoiL2dyb3Vwcy80OTI3MDMyOTc1MjU0MDYiLCJzIjoibSJ9; wd=1351x672; ; c_user=100035861824006; xs=34%3AUXzkKRuqOIA2Rw%3A2%3A1568036259%3A629%3A12765; fr=0SWZUCvrUfbSKcUnY.AWWLqiiy1lPyMqbpaUiur3vtvWY.BdSI35.hv.AAA.0.0.BddlWj.AWWohHjP");
-
-
-$test="wall";
-
-include "tests/test_".$test.".php";
-
 ?>
